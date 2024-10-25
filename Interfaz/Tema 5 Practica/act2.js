5bc77a2b4cd112668a9457383bd4eea52c46580b
mmm0129Medac <148538315+mmm0129Medac@users.noreply.github.com>
2024-10-25 10:51:05 +0200

    worked 25--ver2
    modificación de codigo, sigue sin funcionar

--- Interfaz/Tema 5 Practica/act2.js
+++ Interfaz/Tema 5 Practica/act2.js
@@ -4,8 +4,38 @@ function init(){
     fetch('gastos.json')
         .then(response => response.json())
         .then(data => {
-            datapj = data;
-            filtrerData(gastosJSON);
+            gastosJSON = data;
+            showBills(gastosJSON);
         });
 }
 
+// Referencia a variales del DOM
+const bills = document.getElementById("bills");
+const tBills = document.getElementById("tBills");
+var nameBill = document.getElementById("nameBill");
+var prizeBill = document.getElementById("prizeBill");
+
+// Método para mostrar los gastos y su total. 
+function showBills(dataBills){
+    var totalBills = 0;
+    //bills.innerHTML = ` `;
+    dataBills.forEach (element => {
+        let li = document.createElement("li");
+        li.innerHTML= `
+            <p>${element.name} : $${element.prize}<p><br>
+        `;
+        bills.append(li);
+        totalBills = totalBills + element.prize;    
+    })
+    tBills.innerHTML=`Total acumulado: ${totalBills}$ `
+}
+
+// Método para introducir gastos en la base de datos
+function addBill(){
+    let newBill = gastosJSON.push({"name": nameBill, "prize": prizeBill});
+    console.log(newBill);
+    nameBill.innerHTML = ``;
+    prizeBill.innerHTML = ``;
+    showBills(gastosJSON);
+}
+showBills(gastosJSON);
